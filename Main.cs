26275d424f84a7b91f60ac72f45bc013a31cfb5e
SDraw <sdraw@protonmail.com>
2021-05-15 11:26:15 +0300

    Source upload

---
+++ Main.cs
@@ -0,0 +1,111 @@
+﻿namespace ml_ht
+{
+    public class HeadTurn : MelonLoader.MelonMod
+    {
+        static bool ms_update = false;
+
+        float m_lockedBodyRotation = 0f;
+        static bool ms_lockBodyRotation = false;
+
+        public override void OnUpdate()
+        {
+            if(ms_update)
+            {
+                bool l_ignoreLimit = UnityEngine.Input.GetKey(UnityEngine.KeyCode.LeftControl);
+
+                if(UnityEngine.Input.GetKey(UnityEngine.KeyCode.LeftAlt))
+                {
+                    float l_angle = UnityEngine.Input.mouseScrollDelta.y;
+                    if(l_angle != 0f)
+                    {
+                        var l_neckRotator = VRCPlayer.field_Internal_Static_VRCPlayer_0?.gameObject?.GetComponent<GamelikeInputController>()?.field_Protected_NeckMouseRotator_0;
+                        if(l_neckRotator != null)
+                        {
+                            var l_quat = l_neckRotator.field_Private_Quaternion_0;
+                            var l_vec = l_quat.eulerAngles;
+                            l_vec.z += l_angle * 5f;
+                            if(!l_ignoreLimit)
+                            {
+                                float l_delta = UnityEngine.Mathf.DeltaAngle(l_vec.z, 0f);
+                                if(UnityEngine.Mathf.Abs(l_delta) > 90f)
+                                {
+                                    l_vec.z += (UnityEngine.Mathf.Abs(l_delta) - 90f) * UnityEngine.Mathf.Sign(l_delta);
+                                    l_vec.z = (l_vec.z + 360f) % 360f;
+                                }
+                            }
+                            l_quat.eulerAngles = l_vec;
+                            l_neckRotator.field_Private_Quaternion_0 = l_quat;
+                        }
+                    }
+                }
+
+                if(UnityEngine.Input.GetKeyDown(UnityEngine.KeyCode.LeftAlt))
+                {
+                    if(!ms_lockBodyRotation)
+                    {
+                        var l_transform = VRCPlayer.field_Internal_Static_VRCPlayer_0?.prop_VRCAvatarManager_0?.gameObject?.transform;
+                        if(l_transform != null)
+                        {
+                            m_lockedBodyRotation = l_transform.rotation.eulerAngles.y;
+                            ms_lockBodyRotation = true;
+                        }
+                    }
+                }
+                if(UnityEngine.Input.GetKeyUp(UnityEngine.KeyCode.LeftAlt) || !UnityEngine.Application.isFocused)
+                {
+                    if(ms_lockBodyRotation)
+                    {
+                        var l_transformPlayer = VRCPlayer.field_Internal_Static_VRCPlayer_0?.gameObject?.transform;
+                        var l_transformAvatar = VRCPlayer.field_Internal_Static_VRCPlayer_0?.prop_VRCAvatarManager_0?.gameObject?.transform;
+                        if(l_transformAvatar != null && l_transformAvatar != null)
+                        {
+                            l_transformAvatar.rotation = l_transformPlayer.rotation;
+                        }
+                        ms_lockBodyRotation = false;
+                    }
+                }
+                if(ms_lockBodyRotation)
+                {
+                    var l_playerTransform = VRCPlayer.field_Internal_Static_VRCPlayer_0?.gameObject?.transform;
+                    var l_animatorTransform = VRCPlayer.field_Internal_Static_VRCPlayer_0?.prop_VRCAvatarManager_0?.gameObject?.transform;
+                    if((l_playerTransform != null) && (l_animatorTransform != null))
+                    {
+                        if(!l_ignoreLimit)
+                        {
+                            float l_delta = UnityEngine.Mathf.DeltaAngle(m_lockedBodyRotation, l_playerTransform.eulerAngles.y);
+                            if(UnityEngine.Mathf.Abs(l_delta) > 90f)
+                            {
+                                m_lockedBodyRotation += (UnityEngine.Mathf.Abs(l_delta) - 90f) * UnityEngine.Mathf.Sign(l_delta);
+                                m_lockedBodyRotation = (m_lockedBodyRotation + 360f) % 360f;
+                            }
+                        }
+
+                        var l_quat = l_animatorTransform.rotation;
+                        var l_vec = l_quat.eulerAngles;
+                        l_vec.y = m_lockedBodyRotation;
+                        l_quat.eulerAngles = l_vec;
+                        l_animatorTransform.rotation = l_quat;
+                    }
+                }
+            }
+        }
+
+        // Join patch
+        [Harmony.HarmonyPatch(typeof(NetworkManager), "OnJoinedRoom")]
+        class Patch_NetworkManager_OnJoinedRoom
+        {
+            static void Postfix() => ms_update = true;
+        }
+
+        // Left patch
+        [Harmony.HarmonyPatch(typeof(NetworkManager), "OnLeftRoom")]
+        class Patch_NetworkManager_OnOnLeftRoom
+        {
+            static void Postfix()
+            {
+                ms_update = false;
+                ms_lockBodyRotation = false;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ml_ht")]
+[assembly: AssemblyProduct("ml_ht")]
+[assembly: ComVisible(false)]
+[assembly: Guid("53db91af-2076-4f4f-8560-bc62c64dca52")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+
+[assembly: MelonLoader.MelonInfo(typeof(ml_ht.HeadTurn), "HeadTurn", "1.0.0", "SDraw", "https://github.com/SDraw/ml_ht")]
+[assembly: MelonLoader.MelonGame("VRChat", "VRChat")]
